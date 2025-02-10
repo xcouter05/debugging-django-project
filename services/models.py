953e3797936c75d3bd3f9bca10efd2bd8d532fac
Ella56 <zamani56.elham@gmail.com>
2025-02-10 15:49:02 +0330

    models-part1

--- services/models.py
+++ services/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class services(models.Model):
+    title = models.CharField(max_length=20)
+    content = models.TextField()
+    status = models.BooleanField(default=False)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
