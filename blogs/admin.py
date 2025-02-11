9945530268d61b6221b993f2a4d6fcb899867f4b
Ella56 <zamani56.elham@gmail.com>
2025-02-11 10:11:37 +0330

    Model

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import blog, comments, reply, category, tag
 
 # Register your models here.
+admin.site.register(blog)
+admin.site.register(comments)
+admin.site.register(reply)
+admin.site.register(category)
+admin.site.register(tag)
--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from team.models import team
+from blogs.models import category
+
 
 # Create your models here.
+
+class portfolio(models.Model):
+    agent = models.ForeignKey(team,on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    desc1 = models.TextField()
+    desc2 = models.TextField()
+    category = models.ManyToManyField(category)
+    img1 = models.ImageField(upload_to='portfolio', default='default.png')
+    img2 = models.ImageField(upload_to='portfolio', default='default.png')
+    img3 = models.ImageField(upload_to='portfolio', default='default.png')
+    project_url = models.CharField(max_length=70)
+    client = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return self.title
