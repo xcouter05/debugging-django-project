efcbd418b2b551c33ac68ea48c4c0a1321635eae
YVismeh <Yvisme41148@gmail.com>
2025-02-12 13:40:52 -0600

    update

--- blogs/urls.py
+++ blogs/urls.py
@@ -6,4 +6,5 @@ app_name = "blogs"
 urlpatterns = [
     path('', blogs, name="blogs"),
     path('detail/<int:id>', blog_details, name="blogs_details"),
+    path('category/<str:category>', blogs, name="blogs_category"),
 ]
